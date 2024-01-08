5f2a601c4bc64f6b55eec4829110634f3bf01ec0
mlengse <medtosys@gmail.com>
2024-01-08 12:43:24 +0700

    dfgdfg

--- background.js
+++ background.js
@@ -6,20 +6,20 @@ async function getCurrentTab() {
   return tab;
 }
 
+// let runss = false
 // Called when the url of a tab changes.
 
 chrome.tabs.onUpdated.addListener(async function() {
   let tab = await getCurrentTab();
   // Listen for any changes to the URL of any tab.
   // If the tabs url starts with 'http://192.168.1.99/j-care/visits/add_registrasi'...
-  if (tab.url && (
-    tab.url.includes('http://192.168.100.178/j-care/visits/')
-    || tab.url.includes('http://10.12.10.114:800/j-care/visits/')
-  )) {
+  if (tab.url ) {
+    if(
+      tab.url.includes('/j-care/visits')
+    ) {
+      // Called when the user clicks on the page action.
 
-    // Called when the user clicks on the page action.
-
-    // chrome.action.onClicked.addListener(function() {
+      // chrome.action.onClicked.addListener(function() {
 
       chrome.scripting.executeScript({
         target: {
@@ -34,8 +34,24 @@ chrome.tabs.onUpdated.addListener(async function() {
           "content_script.js",
           ]
       });
-    // });
-    
+
+    } else if(
+      tab.url.includes('j-care/healthcenters/rekap_pemakaian_obat')
+    ) {
+        chrome.scripting.executeScript({
+          target: {
+            tabId: tab.id
+          },
+          files: [ 
+            "jquery-1.11.3.js", 
+            'resep.js'
+          ]
+        });
+
+  
+    }
+
+
   }
 });
 
---
+++ resep.js
@@ -0,0 +1,66 @@
+function waitForElm(elm) {
+  return new Promise( resolve => {
+    var checkExist = setInterval(function() {
+      if ($(elm).length) {
+        // console.log("Exists!");
+         clearInterval(checkExist);
+        resolve(document.querySelector(elm))
+      }
+    }, 100); // check every 100ms
+  })
+}
+
+
+$('input[update="view_pemakaian_obat"]').on('click', async function(evt) {
+  // console.log('clicked')
+  let elm =  await waitForElm('#content')
+  let table = elm.querySelector('table.nested-table')
+  if(table){
+    // console.log('table is ready');
+    let rows = table.querySelectorAll('tr')
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i]
+      let orgCol = document.getElementById(`org${i}`)
+      if(!orgCol){
+        orgCol = row.insertCell(4)
+        orgCol.setAttribute('id', `org${i}`)
+        orgCol.setAttribute('width', '100')
+        orgCol.style.color = '#003399'
+        orgCol.style.textAlign = 'center'
+        orgCol.style.verticalAlign = 'inherit'
+      }
+      if(!orgCol.textContent.length){
+        let links = row.querySelectorAll('a')
+        if(!links.length ){
+          let text = document.createTextNode('Orang')
+          orgCol.appendChild(text)
+        } else {
+          for(let link of links){
+            // console.log(link.textContent);
+            let href = link.getAttribute('href')
+            if( href && href.includes('pasien')){
+              $.ajax(href).done( res => {
+                // console.log()
+                if(!orgCol.textContent.length && res){
+                  let temp = document.createElement('div')
+                  temp.innerHTML = res
+                  let rows = temp.querySelectorAll('tr')
+                  // console.log(rows.length)
+                  let text = document.createTextNode(`${rows.length-1}`)
+                  orgCol.appendChild(text)
+    
+                }
+              })
+            }
+          }
+    
+        }
+  
+      }
+
+    }
+
+  }
+  
+  return false;//Returning false prevents the event from continuing up the chain
+});
